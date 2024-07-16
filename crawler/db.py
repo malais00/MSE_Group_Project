589ce318ee3876b5653948024e0c6190dc839ae0
Hoang An Nguyen <an_nguyen@hotmail.de>
2024-07-16 17:55:46 +0200

    added db function to get content

--- crawler/db.py
+++ crawler/db.py
@@ -82,6 +82,13 @@ class MongoDB:
             queue.append([document["score"], document["counter"], document["url"], document["depth"]])
         return queue
     
+    def getCrawledContent(self):
+        content = []
+        cursor = self.db.crawled.find()
+        for document in cursor:
+            content.append([document["url"], document["content"], document["indexDate"]])
+        return content
+    
     def delete_collection(self, collection_name):
         self.db[collection_name].drop()
         print(f"Collection {collection_name} deleted.")
