9329a7bebc09fb24c49acd62bc89444d8d52ebb2
Julian Borbeck <Julian.borbeck@gmail.com>
2024-07-22 22:47:09 +0200

    fix batch

--- backend/app.py
+++ backend/app.py
@@ -177,21 +177,17 @@ def get_100(query_list):
         else:
             return_df = pd.concat([return_df, query_df], ignore_index=True)
 
-        tsv_buffer = io.StringIO()
-
-        return_df.to_csv(tsv_buffer, sep='\t', index=False, header=False)
-
-        tsv_buffer.seek(0)
-
-        response = Response(tsv_buffer.getvalue(), mimetype="text/tsv")
-
-        response.headers["Content-Disposition"] = "attachment; filename=results.tsv"
-
-        return response
-
     tsv_buffer = io.StringIO()
 
     return_df.to_csv(tsv_buffer, sep='\t', index=False, header=False)
+
+    tsv_buffer.seek(0)
+
+    response = Response(tsv_buffer.getvalue(), mimetype="text/tsv")
+
+    response.headers["Content-Disposition"] = "attachment; filename=results.tsv"
+
+    return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
