df852ae70f12f89f0cbd0f699ba01b5fcad3572f
Julian Borbeck <Julian.borbeck@gmail.com>
2024-07-22 21:55:53 +0200

    update app, fix batch endpoint

--- backend/app.py
+++ backend/app.py
@@ -177,6 +177,14 @@ def get_100(query_list):
         else:
             return_df = pd.concat([return_df, query_df], ignore_index=True)
 
+        tsv_buffer.seek(0)
+
+        response = Response(tsv_buffer.getvalue(), mimetype="text/tsv")
+
+        response.headers["Content-Disposition"] = "attachment; filename=results.tsv"
+
+        return response
+
     tsv_buffer = io.StringIO()
 
     return_df.to_csv(tsv_buffer, sep='\t', index=False, header=False)
